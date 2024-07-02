00270d6a19f26c2190d450a1d5806cf2238b9585
barangievans <barangievans@gmail.com>
2024-07-02 16:35:48 +0300

    review-strings

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper'; // Replace with actual user name
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
